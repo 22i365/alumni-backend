a4f42bc977ebe72043d7b3fa6fc6125d919cb5c3
Shriram <22i365@psgtech.ac.in>
2025-02-09 23:31:40 +0530

    Your commit message

--- server.js
+++ server.js
@@ -43,4 +43,4 @@ const alumniRoutes = require("./routes/alumniRoutes");  // Import routes
 app.use("/api/alumni", alumniRoutes);  // Correct usage of routes
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT,'0.0.0.0', () => console.log(`Server running on port ${PORT}`));
